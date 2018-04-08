0946bea1c37ad8efb1702f2b986d1acbba6f294e
jbhamard <jean-benoit.hamard@wifirst.fr>
2018-04-08 19:42:18 +0000

    set up node_port, google API calls config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const mongoConnexion = process.env.MONGO_CONNECTION
+const googleVisionApiUrl = process.env.GOOLGE_API_VISION_URL
+const googleVisionApiKey = process.env.GOOGLE_API_KEY
+const nodePort = process.env.NODE_PORT
 
-export { mongoConnexion }
+export { mongoConnexion, googleVisionApiUrl, googleVisionApiKey, nodePort }
